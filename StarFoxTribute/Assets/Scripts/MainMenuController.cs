b3beb19451b8190d9d43f3affdcbca9e15dce321
paltet <pau.altet.pujol@hotmail.com>
2020-05-22 22:57:08 +0200

    Main menu

---
+++ StarFoxTribute/Assets/Scripts/MainMenuController.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuController : MonoBehaviour
+{
+    public void Play(){
+        SceneManager.LoadScene("Scenes/Level1");
+    }
+
+    public void Quit(){
+        Application.Quit();
+    }
+}
