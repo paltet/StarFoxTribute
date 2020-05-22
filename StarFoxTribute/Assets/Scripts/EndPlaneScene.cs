98011113c17c78ff65536274841e7ce301c8010d
Joan Muntaner <joanmuntaner1999@gmail.com>
2020-05-23 00:53:30 +0200

    ID-0023 Level Finish

---
+++ StarFoxTribute/Assets/Scripts/EndPlaneScene.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class EndPlaneScene : MonoBehaviour
+{
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.CompareTag("Player"))
+        {
+            // O fer el que vulguem realment
+            SceneManager.LoadScene("Scenes/Menu");
+        }
+    }
+}
