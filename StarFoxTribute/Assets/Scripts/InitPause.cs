fd99418d1b5e62c4fe0da235fde98f746cb54712
Joan Muntaner <joanmuntaner1999@gmail.com>
2020-05-23 15:01:44 +0200

    PauseAtStart

---
+++ StarFoxTribute/Assets/Scripts/InitPause.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InitPause : MonoBehaviour
+{
+    public float duration;
+    void Start()
+    {
+        float pauseEndTime = Time.realtimeSinceStartup + duration;
+        Time.timeScale = 0;
+        while(Time.realtimeSinceStartup < pauseEndTime) {
+        }
+        Time.timeScale = 1;
+    }
+}
