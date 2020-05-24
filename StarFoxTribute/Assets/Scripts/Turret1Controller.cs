f557f8ee02bbb5cbd5ba0ab7cc041038fd67666e
paltet <pau.altet.pujol@hotmail.com>
2020-05-24 17:30:01 +0200

    Lot

---
+++ StarFoxTribute/Assets/Scripts/Turret1Controller.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Turret1Controller : MonoBehaviour
+{
+
+    public float range;
+    public GameObject laserPrefab;
+    public Transform shootingPoint;
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 prediction = GetComponent<SpaceshipPredictor>().Prediction();
+
+        if (Vector3.Distance(prediction, transform.position) <= range){
+            transform.LookAt(prediction);
+            Shoot();
+        }
+    }
+
+    void Shoot(){
+        Instantiate(laserPrefab, shootingPoint.position, shootingPoint.rotation);
+        Debug.Log("shoot");
+    }
+}
