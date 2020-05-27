a1bf9f1f34441847b423276e50e22dec4bd6dda8
Joan Muntaner <joanmuntaner1999@gmail.com>
2020-05-27 19:50:00 +0200

    CleanerPlane

---
+++ StarFoxTribute/Assets/Scripts/CleanerPlane.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CleanerPlane : MonoBehaviour
+{
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.CompareTag("Asteroid") || other.gameObject.CompareTag("Turret") || other.gameObject.CompareTag("Enemy"))
+        {
+            Destroy(other.gameObject);
+        }
+    }
+}
